c85beace97b2e3e6f46a579d47e9a4be20b81167
shrivat <86210465+shrivat@users.noreply.github.com>
2021-07-04 20:10:52 -0400

    Add files via upload

---
+++ js/CannonBall.js
@@ -0,0 +1,32 @@
+class CannonBall{
+    constructor(x, y){
+        var options ={
+            restitution:0.8,
+            density:1,
+            friction:1,
+            isStatic:true       
+            }
+            this.r=40
+            this.body=Bodies.circle(x, y, this.r, options)
+            this.image=loadImage("./assets/cannonball.png")
+            World.add(world,this.body)
+        }
+        display(){
+            var Angle = this.body.angle
+            var pos=this.body.position
+            push()
+            translate(pos.x, pos.y)
+            rotate(Angle)
+            imageMode(CENTER)
+            image(this.image,0,0, this.r, this.r)
+            pop()
+            }
+            shoot(){
+                var velocity=p5.Vector.fromAngle(cannon.angle)
+                velocity.mult(20)
+                Matter.Body.setStatic(this.body,false)
+                Matter.Body.setVelocity(this.body,{
+                    x:velocity.x, y:velocity.y
+                })
+            }
+        }
